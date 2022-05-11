22bc9a18cd28023793cb9fdd92e5b195c02ba689
Benson Chong <benson@machuno.com>
2022-05-11 08:55:16 -0700

    added module of helper functions for BN permutation

---
+++ ODA_network_federation.py
@@ -0,0 +1,296 @@
+import pandas as pd
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import sklearn
+import pomegranate as pg
+
+import random
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import train_test_split
+import networkx as nx
+from pomegranate.utils import plot_networkx
+from sklearn.metrics import roc_auc_score
+import sys
+import mygene
+import os
+from sklearn.model_selection import train_test_split
+
+### Helper functions ###
+
+def reverse(a_list):
+    '''
+    Reverses order of a list
+
+    INPUT
+    a_list -- any list of elements
+
+    RETURN
+    rev_list -- a_list in reverse order
+    '''
+    rev_list = []
+    for i in reversed(a_list):
+        rev_list.append(i)
+
+    return rev_list
+
+def gen_constraints(a_list):
+    '''
+    Generates a constraint graph of all possible edges between nodes
+
+    INPUT
+    a_list - list of all nodes
+
+    RETURN
+    constraints - DiGraph object of constraint graph
+    '''
+    import networkx as nx
+    from pomegranate.utils import plot_networkx
+
+    constraints = nx.DiGraph()
+    constraints.add_nodes_from(a_list)
+
+    for i in range(len(a_list)):
+        for j in range(len(a_list)):
+            if i == j or i>j:
+                continue
+            constraints.add_edge(a_list[i],a_list[j])
+
+    return constraints
+
+def feature_select(dict_pnode, n_min, n_max):
+    '''
+    Randomly selects features based within range (n_min,n_max) based on probability in dictionary
+
+    INPUT
+    dict_pnode - dictionary of gene:probability of selection
+    n_min - minimum number of nodes
+    n_max - maximum number of nodes
+
+    RETURN
+    select - list of genes
+    '''
+    # Select N nodes within range n_min to n_max
+    i = True
+    while i is True:
+        n = np.random.randint(n_min, n_max)
+        select = random.choices(list(dict_pnode.keys()), weights=dict_pnode.values(), k=n)
+        i = len(select) != len(set(select))
+
+    return select
+
+# Re-index nodes to keep
+def reconstruct_bn(x_train, x_test, structure, outcome_node,select):
+    '''
+    Reconstructs Bayesian Network that removes any connections that are not directly connected to outcome
+
+    INPUT
+    structure - structure object from BayesianNetwork() model
+    outcome_node - integer specifying node assigned as outcome
+
+    RETURN
+    df_sub - Pandas dataframe subset of kept nodes
+    new_structure_list - new structure object for BayesianNetwork() model
+    '''
+
+    # Initialize list of nodes to keep
+    keep = [outcome_node]
+    structure_list = list(structure)
+
+    # Traverse network to learn which nodes to keep
+    i = 0
+    for node in structure_list:
+        if outcome_node != i:
+            if outcome_node in node:
+                keep.append(i)
+            elif i in structure_list[outcome_node]:
+                keep.append(i)
+        i +=1
+
+
+    # New structure and subset of data
+    structure = tuple([structure_list[i] for i in keep])
+    x_train_sub = x_train[select].iloc[:,keep]
+    x_test_sub = x_test[select].iloc[:,keep]
+
+    # Reindexing node numbers using a dictionary
+    key = keep.copy()
+    key.sort()
+    val = [i for i in range(len(keep))]
+
+    reindex_dict = {key[i]: val[i] for i in range(len(key))}
+
+    subset_ind = []
+    for i in range(len(keep)):
+        subset_ind.append(reindex_dict.get(keep[i]))
+
+    new_structure_list = []
+    for i in range(len(structure_list)):
+        if i not in keep:
+            pass
+        else:
+            new_edge = []
+            for node in structure_list[i]:
+                if node in reindex_dict:
+                    node = reindex_dict.get(node)
+                    new_edge.append(node)
+                else:
+                    new_edge = tuple()
+            new_structure_list.append(tuple(new_edge))
+
+    return tuple((x_train_sub, x_test_sub)),tuple(new_structure_list)
+
+def remove_state(train, test):
+    '''
+    Removes any state that appears in the test set that was not in training set
+
+    INPUT
+    train - Pandas df train set
+    test - Pandas df test set
+
+    RETURN
+    test - transformed Pandas df test set
+    '''
+    rm_dict = {}
+
+    for (col, val) in train.iteritems():
+        if col =='gleason_score':
+            continue
+
+        remove = list(set(test[col])-set(val))
+        if remove != []:
+            rm_dict[col] = remove
+
+    for col,rm in rm_dict.items():
+        test = test[~test[col].isin(rm)]
+
+    return test
+
+# Alternate: produce keys list for every state
+
+def keys_list(x):
+    '''
+    Acquires all possible keys for each state
+
+    INPUT
+    x - matrix of discretized values
+    RETURN
+    keys_list - ordered list of lists of all possible keys
+    '''
+    keys_list = []
+
+    for i in x:
+        keys_list.append(set(x[i]))
+
+    return keys_list
+
+### PERMUTATION WRAPPER ###
+# Wrapper function to run permutations
+
+def permute_bn(df,max_iter=1000,m=3,n_min=5,n_max=20):
+
+    # Hardcode 0 as outcome node
+    outcome_node = 0
+    scores, models = [],[]
+
+    # Assume target column is first
+    X = df.iloc[:,1:]
+    y = df.iloc[:,1]
+
+    x_train, x_test, y_train, y_test = train_test_split(X.to_numpy(), y.to_numpy(), test_size=0.33, random_state=42)
+    x_train = pd.DataFrame(x_train, columns = list(X.columns))
+    x_test = pd.DataFrame(x_test, columns = list(X.columns))
+
+
+    selector = SelectKBest(chi2, k='all').fit(X.iloc[:,1:], X['gleason_score'])
+    x_rank = selector.scores_
+    x_rank = np.nan_to_num(x_rank, nan=0.001)
+    x_rank /= 1000
+    selector.feature_names_in_
+    dict_pnode = {selector.feature_names_in_[i]: x_rank[i] for i in range(len(x_rank))}
+
+    for i in range(max_iter):
+
+        # selects features from ENTIRE dataset (before partition)
+        select = feature_select(dict_pnode,n_min,n_max)
+        select.insert(0,'gleason_score')
+
+        # Constraint graph
+        rev_select = reverse(select)
+        constraints = gen_constraints(rev_select)
+
+        # Build BayesianNetwork with constraint graph
+        print('Building model...')
+        model = pg.BayesianNetwork.from_samples(x_train[select], algorithm='greedy', max_parents=m,
+                                                constraint_graph=constraints)
+
+        print('Reconstructing model...1/2')
+        new_df, new_struct = reconstruct_bn(x_train[select], x_test[select], model.structure,outcome_node,select)
+        x_train_sub, x_test_sub = new_df[0], new_df[1]
+
+        y =  x_test_sub['gleason_score']
+        x_test_sub['gleason_score'] = None
+
+        keys = keys_list(pd.concat([x_train_sub,x_test_sub]))
+
+        # TEST ZONE #
+
+        # END TEST ZONE #
+
+        # Change state names to gene symbols
+        mg = mygene.MyGeneInfo()
+        gene_sym = mg.querymany(list(x_train_sub.columns),scopes='ensembl.gene',fields='symbol',species='human')
+
+        state_names = ['gleason_score']
+        for j in gene_sym:
+            for key in j:
+                if key == 'symbol':
+                    state_names.append(j.get(key))
+
+        print('Reconstructing model...2/2')
+        re_model = pg.BayesianNetwork.from_structure(x_train_sub, new_struct, state_names=state_names, keys=keys)
+        y_pred = np.array(re_model.predict(x_test_sub.to_numpy()))[:,0]
+
+
+        ### Update scores ###
+
+        # Keep record of last 5 scores
+        curr_score = roc_auc_score(y,y_pred)
+        scores.append(curr_score)
+        models.append(re_model)
+
+        print("Round",i+1,': ',curr_score)
+
+        # Update probability of choosing node by +/- 5%
+        if len(scores) > 1 and scores[len(scores)-1] > scores[len(scores)-2]:
+            for gene in select:
+                if gene == 'gleason_score':
+                    continue
+                dict_update = {gene:dict_pnode[gene] + 0.05}
+                dict_pnode.update(dict_update)
+        elif len(scores) > 1 and scores[len(scores)-1] < scores[len(scores)-2]:
+            for gene in select:
+                if gene == 'gleason_score':
+                    continue
+                dict_update = {gene:dict_pnode[gene] - 0.05}
+                dict_pnode.update(dict_update)
+
+        # Keep total scores list to 5 elements
+        if len(scores) > 5:
+            scores.pop(0)
+
+        # If current score is less than the mean of 5 previous scores, end loop
+        if len(scores) == 5 and curr_score < np.mean(scores[0:3]):
+            print('Training complete.')
+            max_index = max(range(len(scores)), key=scores.__getitem__)
+
+            final_score = scores[max_index]
+            final_model = models[max_index]
+            break
+
+    print('\nThe top scoring model scored ',final_score)
+
+    plt.figure(figsize=(14, 10))
+    final_model.plot()
+    plt.show()
